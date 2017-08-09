521404e64375891dd1133788ab5875ab12ea5c54
arunvm <arunvmathew95@gmail.com>
2017-08-09 23:12:25 +0530

    Added Javascript

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+
+
+var img = document.getElementById("madi");
+var marginLeft1=0;
+function moveRight() {
+    marginLeft1 =marginLeft1 + 1;
+    img.style.marginLeft = marginLeft1 + 'px';
+}
+
+img.onclick = function(){
+    var interval= setInterval(moveRight,50);
+}
